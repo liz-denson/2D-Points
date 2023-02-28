ead5f83b63abc3737d8b2d47439087bb998da74b
liz-denson <123184723+liz-denson@users.noreply.github.com>
2023-02-28 14:44:12 -0500

    Update 2D Points.py

--- 2D Points.py
+++ 2D Points.py
@@ -1,16 +1,17 @@
-#######################################################################################################
+########################################################################################################
+
 # Word Search (Grid Class)
 # Liz Denson & Caroline Holland
 # Last modified on 2023-02-28
 #
 # An object-oriented program that utilizes a GUI and includes a 2D Point class instantiating randomly
 # generated points within a specified coordinate range, and plotting those points on a canvas.
-#######################################################################################################
+########################################################################################################
 
 # Import libraries
+from tkinter import *
+from random import randint, choice
 import math
-import tkinter
-import random
 
 # Point class
 class Point:
@@ -46,7 +47,8 @@ class Point:
     # Calculate the distance between two points and takes another Point as an argument
     def dist(self, other_point):
         # Distance formula to calculate the distance between the two points
-        return math.sqrt((self._x - other_point.get_x()) ** 2 + (self._y - other_point.get_y()) ** 2)
+        return math.sqrt((self._x - other_point.get_x()) ** 2 +
+                         (self._y - other_point.get_y()) ** 2)
 
     # Calculate the midpoint between two points and takes another Point as an argument
     def midpt(self, other_point):
@@ -60,27 +62,63 @@ class Point:
     def __str__(self):
         # Returns a string in the format (x,y)
         return "({}, {})".format(self._x, self._y)
-    
-# CoordinateSystem class that inherits from Tkinter's Canvas class
-class CoordinateSystem(tk.Canvas):
-    # Class varaibles
-    # Class variable for the radius of the plotted points
-    POINT_RADIUS = 0
-    # Class variable for the possible colors of the plotted points
-    POINT_COLORS = [ "black", "red", "green", "blue", "cyan", "yellow", "magenta" ]
-    # Class variable that sets the width to 800
-    WIDTH = 800
-    # Class variable that sets the height to 800
-    HEIGHT = 800
-    # Class variable that sets the number of points to 5000
-    NUM_POINTS = 5000
+
+# CoordinateSystem class varaibles
+# Class variable that sets the width to 800
+WIDTH = 800
+# Class variable that sets the height to 800
+HEIGHT = 800
+# Class variable for the possible colors of the plotted points
+POINT_COLORS = ['black', 'red', 'green', 'blue', 'cyan', 'yellow', 'magenta']
+# Class variable for the radius of the plotted points
+POINT_RADIUS = 0
+# Class variable that sets the number of points to 5000
+NUM_POINTS = 5000
+
+# The CoordinateSystem class inheriting from Tkinter's Canvas class
+class CoordinateSystem(Canvas):
     # Constructor for the CoordinateSystem class
-    def __init__(self, master):
-        # Instance variables
-        # Initializes a new canvas with a white background
-        Canvas.__init__(self, master, bg="white")
-        # Pack the canvas to fill the entire Tkinter window
-        self.pack(fill=tk.BOTH, expand=True)
+    def __init__(self, master, width=WIDTH, height=HEIGHT, num_points=NUM_POINTS):
+        # Initialize a canvas object with the width, height, and white background
+        super().__init__(master, width=width, height=height, bg='white')
+        # Pack the canvas object to fill the entire Tkinter window
+        self.pack(fill=BOTH, expand=True)
         # Instance variable for the number of points to plot
         self.num_points = num_points
-        
+
+    # Method to plot a single point on the canvas
+    def plot(self, point, color):
+        # Get the x and y coordinates of the point
+        x, y = point.get_x(), point.get_y()
+        # Create an oval with a radius of POINT_RADIUS centered
+        # on the point's x and y coordinates with the specified color
+        self.create_oval(x, y, x + POINT_RADIUS, y + POINT_RADIUS, fill=color)
+
+    # Method to plot multiple random points on the canvas
+    def plotPoints(self, n):
+        # Plot n random points on the canvas within the specified width and height
+        for i in range(n):
+            x = randint(0, WIDTH - 1)
+            y = randint(0, HEIGHT - 1)
+            self.plot(x, y)
+    
+    # Method to plot a single point on the canvas with specified x and y coordinates
+    def plot(self, x, y):
+        # Select a random color from the POINT_COLORS list
+        color = choice(POINT_COLORS)
+        # Create an oval with a radius of POINT_RADIUS centered
+        # on the specified x and y coordinates with the specified color
+        self.create_oval(x, y, x + POINT_RADIUS * 2, y + POINT_RADIUS * 2, outline=color)
+
+# Create a Tkinter window
+window = Tk()
+# Set the geometry of the window to the specified WIDTH and HEIGHT
+window.geometry("{}x{}".format(WIDTH, HEIGHT))
+# Set the title of the window
+window.title("Check out these points!")
+# Create an instance of the CoordinateSystem class on the Tkinter window
+p = CoordinateSystem(window)
+# Call the plotPoints() method to plot NUM_POINTS random points on the canvas
+p.plotPoints(NUM_POINTS)
+# Start the Tkinter event loop
+window.mainloop()
