44fe4272f9759db3cc056d35bd456512a27e2d10
liz-denson <123184723+liz-denson@users.noreply.github.com>
2023-02-28 12:15:14 -0500

    Add files via upload

---
+++ 2D Points.py
@@ -0,0 +1,13 @@
+###############################################################################
+# Word Search (Grid Class)
+# Liz Denson & Caroline Holland
+# Last modified on 2023-02-28
+#
+# An object-oriented program that utilizes a GUI and includes a 2D Point class
+# instantiating randomly generated points within a specified coordinate range,
+# and plotting those points on a canvas.
+###############################################################################
+
+# Point class
+class Point:
+    def __init__
